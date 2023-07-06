b6a5db3d0398c09a20234892f3d1f7d85b10747a
Ranjan Majumdar <ranjanm1@gmail.com>
2023-07-06 13:04:56 +0100

    boolean if-else-elif

---
+++ src/hello-world/boolif.py
@@ -0,0 +1,9 @@
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+# test expression
+if a < b:
+    print("First number is small " + a)
+elif a > b:
+    print("Second number is small " + b)
+else:
+    print("Both numbers are equal")
